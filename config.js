2cf1982c8c9aac81e9db21f53173e3a5864147ae
dynin99 <163668096+dynin99@users.noreply.github.com>
2024-03-16 22:29:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
   TOKEN: "",
-  ownerID: ["1004206704994566164", ""],
+  ownerID: ["394505972347043850", ""],
   botInvite: "",
   supportServer: "",
-  mongodbURL: "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
-  status: 'RTXX GG',
+  mongodbURL: "mongodb+srv://DYNIN:DYNIN99@cluster0.yzmv51o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  status: 'DyNin GG',
   commandsDir: './commands',
   language: "en",
   embedColor: "00fbff",
@@ -13,7 +13,7 @@ module.exports = {
 
   sponsor: {
     status: true,
-    url: "https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A",
+    url: "",
   },
 
   voteManager: {
